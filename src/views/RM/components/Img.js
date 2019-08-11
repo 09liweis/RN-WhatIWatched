f93ddbe964094c177ac2fc070673b62457b28a72
Sam Li <weisen.li@hotmail.com>
2019-08-11 10:54:34 -0400

    modify Img use ImageBackground

--- src/views/RM/components/Img.js
+++ src/views/RM/components/Img.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, FlatList, TouchableOpacity, Button} from 'react-native';
-
+import {Platform, StyleSheet, Text, View, ImageBackground, TouchableOpacity, Button} from 'react-native';
+import Label from './Label'
 export default class Img extends Component {
   constructor(props) {
     super(props);
@@ -11,17 +11,20 @@ export default class Img extends Component {
   }
   render() {
     const {img} = this.props;
+    let lbl;
+    if (img.lbl) {
+      lbl = <Label lbl={img.lbl}/>
+    }
     return(
-      <TouchableOpacity onPress={()=> console.log('test')}>
-        <View>
-          <Image style={styles.image} source={{uri:img.url}}/>
-        </View>
-      </TouchableOpacity>
+      <ImageBackground style={styles.img} source={{uri:img.url}}>
+        {lbl}
+      </ImageBackground>
     );
   }
 }
 const styles = StyleSheet.create({
-  image:{
+  img:{
+    position:'relative',
     aspectRatio:4/3,
     // width:150,
     height:100
