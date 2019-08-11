03740ccd70aecf796ab1445f79455181ff2a3c17
Sam Li <weisen.li@hotmail.com>
2019-08-10 22:59:21 -0400

    add style file

---
+++ src/views/RM/components/ElmStyle.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+export default ElmStyle = StyleSheet.create({
+  view:{
+    padding:20,
+    marginBottom:20
+  },
+  viewOne:{
+    flex:1,
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center'
+  },
+  viewTl:{
+    fontSize:20,
+    fontWeight:'bold'
+  }
+})
