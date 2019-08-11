2195c5286ce0c559501ae431f95702546e982c6c
Sam Li <weisen.li@hotmail.com>
2019-08-11 10:54:45 -0400

    add feeds.js

---
+++ src/views/RM/feeds.js
@@ -0,0 +1,112 @@
+export default feeds = [
+  {
+    id:1,
+    tl:'Upgrade to Premium VIP',
+    sbtl:'Get more advanced features',
+    bg:'#ccc',
+    thumb:{tp:2,url:'https://www.realmaster.com/img/icon_index_sale.png'},
+    tags:[{tl:'Tag'}]
+  },
+  {
+    id:1,
+    tl:'This is an Adddddddd',
+    color:'#fff',
+    bg:'#efdfdf',
+    tag:{tl:'Detail',style:2}
+  },
+  {
+    id:1,
+    tl:'This is an Ad',
+    sbtl:'Get more info',
+    color:'#fff',
+    bg:{url:'https://img.realmaster.com/mls/1/766/C4420766.jpg?5552753'}
+  },
+  {
+    id:2,
+    tl:'Cost of living Comparison between Markham and Toronto',
+    imgs:[
+      {
+        url:'https://img.realmaster.com/mls/1/766/C4420766.jpg?5552753',
+        lbl:{
+          tl:'AD'
+        }
+      },
+      {
+        url:'https://img.realmaster.com/mls/1/766/C4420766.jpg?5552753',
+        lbl:{
+          tl:'TOP',
+          pos:2
+        }
+      },
+      {
+        url:'https://img.realmaster.com/mls/1/766/C4420766.jpg?5552753',
+        lbl:{
+          tl:'AD',
+          pos:3
+        }
+      }
+      ],
+    txt:'This is a description,This is a description,This is a description,This is a description,This is a description.',
+    tags:[
+      {
+        tl:'Hot'
+      }
+    ]
+  },
+  {
+    id:2,
+    tl:'Don Mills - North York',
+    imgs:[
+      {
+        url:'https://img.realmaster.com/mls/1/766/C4420766.jpg?5552753',
+        lbl:{
+          tl:'AD',
+          pos:4
+        }
+      },
+      {
+        url:'https://img.realmaster.com/mls/1/766/C4420766.jpg?5552753',
+        lbl:{
+          tl:'TOP',
+          pos:5
+        }
+      },
+      {
+        url:'https://img.realmaster.com/mls/1/766/C4420766.jpg?5552753',
+        lbl:{
+          tl:'AD',
+          pos:6
+        }
+      }
+      ],
+    txt:'This is a description,This is a description,This is a description,This is a description,This is a description.',
+    tags:[
+      {
+        tl:'Canada'
+      }
+    ]
+  },
+  {
+    id:2,
+    tl:'65 Broadway Ave Condos',
+    imgs:[
+      {
+        url:'https://img.realmaster.com/mls/1/766/C4420766.jpg?5552753',
+        lbl:{
+          tl:'AD',
+          pos:4
+        }
+      }
+      ],
+    txt:'10 for Sales, 20 for Rent',
+    tags:[
+      {
+        tl:'Recommand'
+      },
+      {
+        tl:'Don Valley North',
+        color:'#ccc',
+      }
+    ]
+  },
+]
