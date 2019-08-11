f50683cc3f8593aef5602fe9e4c5194228bd9fec
Sam Li <weisen.li@hotmail.com>
2019-08-11 14:42:04 -0400

    import feeds

--- src/views/RM/RMHomePage.js
+++ src/views/RM/RMHomePage.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Image, FlatList, TouchableOpacity, Button} from 'react-native';
+import feeds from './feeds'
 import Block1 from './components/Block1';
 import Block2 from './components/Block2';
 
@@ -7,12 +8,7 @@ export default class RMHomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      feeds: [
-        {id:1,tl:'Upgrade to Premium VIP',sbtl:'Get more advanced features',bg:'#ccc',thumb:{tp:2,url:'https://www.realmaster.com/img/icon_index_sale.png'},tags:[{tl:'Tag'}]},
-        {id:1,tl:'This is an Adddddddd',color:'#fff',bg:'#efdfdf',tag:{tl:'Detail',style:2}},
-        {id:1,tl:'This is an Ad',sbtl:'Get more info',color:'#fff',bg:{url:'https://img.realmaster.com/mls/1/766/C4420766.jpg?5552753'}},
-        {id:2,tl:'Cost of living Comparison between Markham and Toronto',imgs:[{url:'https://img.realmaster.com/mls/1/766/C4420766.jpg?5552753',}]},
-      ]
+      feeds:feeds
     }
   }
   componentDidMount() {
@@ -31,9 +27,9 @@ export default class RMHomePage extends Component {
           data={feeds}
           keyExtractor={item => item.id.toString()}
           renderItem={({item}) =>
-            {if (item.id == 1) {
+            {if (item.tp == 1) {
               return <Block1 feed={item}/>;
-            } else if (item.id == 2) {
+            } else if (item.tp == 2) {
               return <Block2 feed={item}/>;
             }}
           }
