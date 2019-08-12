9377e948ca9a22d951e903fac1df2e5f3f6ed75e
Sam Li <weisen.li@hotmail.com>
2019-08-11 23:03:58 -0400

    add propcontent on Img

--- src/views/RM/components/Img.js
+++ src/views/RM/components/Img.js
@@ -11,7 +11,7 @@ export default class Img extends Component {
   }
   render() {
     const {img,numImgs} = this.props;
-    let lbl;
+    let lbl,prop;
     if (img.lbl) {
       lbl = <Label lbl={img.lbl}/>
     }
@@ -21,9 +21,25 @@ export default class Img extends Component {
     if (numImgs == 1) {
       ratio.height = 150
     }
+    if (numImgs == 2) {
+      ratio.height = 120
+    }
+    if (img.prop) {
+      prop = (
+        <View>
+          <Text style={[styles.propTxt,styles.propLp]}>{img.prop.lp}</Text>
+          <Text style={[styles.propTxt,styles.propSp]}>{img.prop.sp}</Text>
+          <Text style={[styles.propTxt,styles.dom]}>DOM: {img.prop.dom}</Text>
+          <Text style={[styles.propTxt]}>Tax: {img.prop.tax}</Text>
+          <Text style={[styles.propTxt]}>{img.prop.addr}</Text>
+          <Text style={[styles.propTxt]}>{img.prop.sid}</Text>
+        </View>
+      );
+    }
     return(
       <ImageBackground style={[styles.img,ratio]} source={{uri:img.url}}>
         {lbl}
+        {prop}
       </ImageBackground>
     );
   }
@@ -31,6 +47,19 @@ export default class Img extends Component {
 const styles = StyleSheet.create({
   img:{
     position:'relative',
-    height:100
+    height:100,
+    padding:10
+  },
+  propTxt:{
+    color:'#fff'
+  },
+  propLp:{
+    fontWeight:'bold'
+  },
+  propSp:{
+    textDecorationLine:'line-through'
+  },
+  dom:{
+    color:'#fcae28'
   }
 })
