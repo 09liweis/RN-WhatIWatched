4b4692cc66219c606e1c478f42d66bf53acadb2f
Sam Li <weisen.li@hotmail.com>
2019-08-12 16:33:04 -0400

    add profile component

---
+++ src/views/RM/components/Profile.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Image, FlatList, TouchableOpacity, Button} from 'react-native';
+import ElmStyle from './ElmStyle'
+import Thumbnail from './Thumbnail'
+export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+    
+  }
+  componentDidMount() {
+
+  }
+  render() {
+    const {profile} = this.props;
+    return(
+      <TouchableOpacity onPress={()=> console.log('test')}>
+        <View style={styles.profile}>
+          <Thumbnail thumb={profile.thumb} />
+          <View style={{marginLeft:5}}>
+            <Text style={ElmStyle.viewTl}>{profile.tl}</Text>
+            <Text>{profile.sbtl}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  profile:{
+    flexDirection:'row',
+    alignSelf:'flex-start',
+    alignItems:'center'
+  }
+})
