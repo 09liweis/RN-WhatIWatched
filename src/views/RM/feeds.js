1d1c5f17b168dd9b5fde55d85a3b685c586dbf13
Sam Li <weisen.li@hotmail.com>
2019-08-11 23:04:57 -0400

    add propcontent to feeds

--- src/views/RM/feeds.js
+++ src/views/RM/feeds.js
@@ -12,7 +12,6 @@ export default feeds = [
     id:2,
     tp:1,
     tl:'This is an Adddddddd',
-    color:'#fff',
     bg:'#efdfdf',
     tag:{tl:'Detail',style:2}
   },
@@ -64,9 +63,21 @@ export default feeds = [
     imgs:[
       {
         url:'https://img.realmaster.com/mls/1/766/C4420766.jpg?5552753',
-        lbl:{
-          tl:'AD',
-          pos:4
+        // lbl:{
+        //   tl:'AD',
+        //   pos:4
+        // },
+        prop:{
+          lp:'$783,000',
+          sp:'$778,000',
+          dom:6,
+          tax:'5564/2019',
+          addr:'140 Parkhurst Blvd',
+          type:['Detached','House'],
+          bdrms:4,
+          bthrms:3,
+          gr:4,
+          sid:'C344543'
         }
       },
       {
@@ -112,8 +123,8 @@ export default feeds = [
     id:7,
     tp:4,
     tl:'人民币破7 全球资产配置的重新洗牌',
-    txt:'10 for Sales, 20 for Rent',
-    img:{
+    txt:'Text area 02',
+    thumb:{
       url:'http://mmbiz.qpic.cn/mmbiz_png/ibj6cS3UulvPxslVcnawtBMGlBaRf1Sk2d92CU6wkxdria1qXic64jwXMREYoH2UpVicKPNhy1Iibh7ozf2cbwsue1A/640?wx_fmt=png',
     },
     tags:[
