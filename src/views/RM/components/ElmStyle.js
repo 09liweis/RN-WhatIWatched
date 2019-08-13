19d3e16d21d531aa193f0046e4b8684682806ab7
Sam Li <weisen.li@hotmail.com>
2019-08-13 13:04:43 -0400

    add margin left right for block

--- src/views/RM/components/ElmStyle.js
+++ src/views/RM/components/ElmStyle.js
@@ -3,6 +3,8 @@ import {StyleSheet} from 'react-native';
 export default ElmStyle = StyleSheet.create({
   view:{
     padding:20,
+    marginLeft:20,
+    marginRight:20,
     marginBottom:20
   },
   viewOne:{
