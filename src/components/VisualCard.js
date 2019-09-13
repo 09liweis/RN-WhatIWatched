14e1322512777386d5f2e190f5d0ceae340336bd
Sam Li <weisen.li@hotmail.com>
2019-09-12 22:28:05 -0400

    style update for visual card

--- src/components/VisualCard.js
+++ src/components/VisualCard.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Image,Linking,TouchableOpacity,Dimensions} from 'react-native';
 
 const {width} = Dimensions.get('window');
-const cardWdith = (width-30)/3
+const cardWdith = (width)/3
 
 export default class Visuals extends Component {
 	static navigationOptions = {
@@ -22,15 +22,15 @@ export default class Visuals extends Component {
 		return (
 			<TouchableOpacity style={styles.card} onPress={() => navigation.navigate('VisualDetail',{visual: v})}>
 				<Image style={styles.VisualImage} source={{uri: v.poster}} />
+				<Text style={[styles.VisualText,styles.progress]}>{v.current_episode}/{v.episodes}</Text>
+				{/* <Text style={[styles.VisualText, styles.VisualTitle]}>{v.title}</Text> */}
 				{/* <View style={styles.VisualDetail}>
-					<Text style={[styles.VisualText, styles.VisualTitle]}>{v.title}</Text>
 					<Text style={[styles.VisualText]}>{v.original_title}</Text>
 					<TouchableOpacity onPress={()=>Linking.openURL('https://movie.douban.com/subject/'+v.douban_id)}>
 						<Text style={styles.VisualText}>豆瓣评分: {v.douban_rating}</Text>
 					</TouchableOpacity>
 					<Text style={styles.VisualText}>IMDB: {v.imdb_rating}</Text>
 					<Text style={styles.VisualText}>{v.release_date}</Text>
-					<Text style={styles.VisualText}>进度: {v.current_episode}/{v.episodes}</Text>
 					<Text style={styles.VisualText}>国家: {v.countries.join(',')}</Text>
 					<Text style={styles.VisualText}>语言: {v.languages.join(',')}</Text>
 				</View> */}
@@ -44,20 +44,21 @@ const styles = StyleSheet.create({
 		backgroundColor:'#fff',
 		flex: 1,
 		flexDirection: 'row',
-		marginTop:15,
-		marginLeft:15,
-		marginRight:15,
+		// marginTop:15,
+		// marginLeft:15,
+		// marginRight:15,
 		elevation,
 		shadowColor: 'black',
 		shadowOffset: { width: 0, height: 0.5 * elevation },
 		shadowOpacity: 0.3,
 		shadowRadius: 0.8 * elevation,
+		position:'relative',
 		width: cardWdith,
 	},
 	VisualImage: {
 		width: cardWdith,
-		height: 200,
-		borderRadius: 5
+		height: 250,
+		// borderRadius: 5
 	},
 	VisualDetail: {
 		padding: 10
@@ -70,5 +71,13 @@ const styles = StyleSheet.create({
 		fontSize: 20,
 		fontFamily:'Roboto',
 		fontWeight:'bold'
+	},
+	progress:{
+		position:'absolute',
+		top:0,
+		left:0,
+		backgroundColor:'#fff',
+		padding:4,
+		borderRadius:2
 	}
 });
