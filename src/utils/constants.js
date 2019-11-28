a0b62014c59ca10c22e54d83827f9abbd5fce09c
Sam Li <weisen.li@hotmail.com>
2019-11-27 21:25:14 -0500

    add douban detail api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,10 +3,12 @@ const API_LIST = API + 'visuals/';
 const API_DETAIL = API + 'visual/';
 const API_INCREASE_EPISODE = API_DETAIL +'increase_episode?id=';
 const API_SEARCH = API + 'search?keyword=';
+const API_DOUBAN_DETAIL = 'https://api.douban.com/v2/movie/subject/{id}?apikey=0df993c66c0c636e29ecbb5344252a4a';
 export {
   API,
   API_LIST,
   API_DETAIL,
   API_SEARCH,
-  API_INCREASE_EPISODE
+  API_INCREASE_EPISODE,
+  API_DOUBAN_DETAIL
 }
