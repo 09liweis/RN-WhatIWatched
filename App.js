222703f49d7c7cef24985f387376d2c14150a7a9
Sam Li <weisen.li@hotmail.com>
2019-12-01 10:55:34 -0500

    display tab screen as home screen

--- App.js
+++ App.js
@@ -7,8 +7,9 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import {createStackNavigator, createAppContainer} from 'react-navigation';
+import {Platform, StyleSheet} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import Home from './src/screens/Home';  //Tab Nav
 
 import Visuals from './src/views/Visuals';
 import VisualDetail from './src/views/VisualDetail';
@@ -21,47 +22,36 @@ export default class App extends Component<Props> {
   }
 }
 
-class Home extends Component<Props> {
-  static navigationOptions = {
-    title: 'What I Watched!',
-    headerStyle: {
-      backgroundColor: '#fff',
-    },
-    headerTintColor:'#090f2b'
-  };
-  render() {
-    return (
-      <View style={styles.container}>
-        <Visuals navigation={this.props.navigation} />
-      </View>
-    );
-  }
-}
-
-const AppStackNavigator = createStackNavigator(
-{
-  Home: Home,
-  VisualDetail: VisualDetail,
-  VisualForm: VisualForm,
-},
-{
-  initialRouteName: 'Home'
-},
-{
-  defaultNavigationOptions: {
-    headerStyle: {
-      backgroundColor: 'orange'
-    }
-  }
-}
-);
-const AppContainer = createAppContainer(AppStackNavigator);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-});
+// class Home extends Component<Props> {
+//   static navigationOptions = {
+//     title: 'What I Watched!',
+//     headerStyle: {
+//       backgroundColor: '#fff',
+//     },
+//     headerTintColor:'#090f2b'
+//   };
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Visuals navigation={this.props.navigation} />
+//       </View>
+//     );
+//   }
+// }
+const AppContainer = createAppContainer(Home);
+// export default createDrawerNavigator({
+//   Home: {
+//     screen: Home,
+//     navigationOptions: {
+//       drawerLabel: 'Home',
+//       drawerIcon: ({ tintColor }) => <Icon name="cog" size={17} />,
+//     }
+//   },
+//   Visuals: {
+//     screen: Visuals,
+//     navigationOptions: {
+//       drawerLabel: 'Visuals',
+//       drawerIcon: ({ tintColor }) => <Icon name="user-circle" size={17} />,
+//     }
+//   }
+// });
