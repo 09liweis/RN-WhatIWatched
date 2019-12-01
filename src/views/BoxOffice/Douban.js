a3cea973b4a5c9755bf5e401f7fcc21847ce3558
Sam Li <weisen.li@hotmail.com>
2019-12-01 12:12:32 -0500

    style douban box office

--- src/views/BoxOffice/Douban.js
+++ src/views/BoxOffice/Douban.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, FlatList, TouchableOpacity, TextInput,Button} from 'react-native';
+import {Platform, StyleSheet, Text, View, FlatList,Image} from 'react-native';
 import {API_DOUBAN_USBO} from '../../utils/constants.js'
 
 export default class Douban extends Component {
@@ -29,13 +29,20 @@ export default class Douban extends Component {
 		const {douban} = this.state;
     return (
       <View>
-				<Text>{douban.title} {douban.date}</Text>
+				<Text style={styles.title}>{douban.title} {douban.date}</Text>
 				<FlatList
 					data={douban.subjects}
 					keyExtractor={item => item.subject.id.toString()}
 					renderItem={({item}) =>
-						<View>
-							<Text>{item.subject.original_title}</Text>
+						<View style={styles.boxoffice}>
+							<Image style={styles.boxofficeImg} source={{uri:item.subject.images.small}}/>
+							<View style={styles.boxofficeInfo}>
+								<Text>{item.subject.title}</Text>
+								<Text>{item.subject.original_title}</Text>
+								<Text>{item.subject.durations.join(',')}</Text>
+								<Text>{item.subject.genres.join(',')}</Text>
+								<Text>${item.box}</Text>
+							</View>
 						</View>
 					}
 					/>
@@ -44,5 +51,20 @@ export default class Douban extends Component {
   }
 }
 const styles = StyleSheet.create({
-  
+	title:{
+		textAlign:'center',
+		fontSize:20
+	},
+  boxoffice:{
+		marginBottom:5,
+		padding:10,
+		flexDirection:'row',
+	},
+	boxofficeImg:{
+		width:100,
+		height:150
+	},
+	boxofficeInfo:{
+		padding:10
+	}
 });
