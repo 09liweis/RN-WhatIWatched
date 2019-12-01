efcbf1c4379159539451c8e35677880746c73c55
Sam Li <weisen.li@hotmail.com>
2019-12-01 11:08:57 -0500

    fix tab issue

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,33 +2,33 @@ import React from 'react';
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import Visuals from '../views/Visuals';
-import DoubanBoxOffice from '../views/DoubanBoxOffice'
+import BoxOffice from '../views/BoxOffice'
 
 const Home = createBottomTabNavigator({
   Visuals: {
-        screen: Visuals,
-        navigationOptions: {
-            title: "Visuals",
-            tabBarIcon: ({ tintColor }) => (
-                <Icon
-                    name="microchip"
-                    size={17}
-                    color={tintColor} />
-            )
-        }
-    },
-    DoubanBoxOffice: {
-        screen: DoubanBoxOffice,
-        navigationOptions: {
-            tabBarLabel: "Douban Box Office",
-            tabBarIcon: ({ tintColor }) => (
-                <Icon
-                    name="memory"
-                    size={17}
-                    color={tintColor} />
-            )
-        }
-    }
+		screen: Visuals,
+		navigationOptions: {
+			title: "Visuals",
+			tabBarIcon: ({ tintColor }) => (
+				<Icon
+					name="microchip"
+					size={17}
+					color={tintColor} />
+			)
+		}
+  },
+	BoxOffice: {
+		screen: BoxOffice,
+		navigationOptions: {
+			tabBarLabel: "Box Office",
+			tabBarIcon: ({ tintColor }) => (
+				<Icon
+					name="memory"
+					size={17}
+					color={tintColor} />
+			)
+		}
+	}
 });
 
 //Issue: the tab navigator needs to be wrapped inside a stack navigator
