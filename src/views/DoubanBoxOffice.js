1f1c9250cbfcd888c89d230b73ff07b74b136da9
Sam Li <weisen.li@hotmail.com>
2019-12-01 10:55:13 -0500

    add box office page

---
+++ src/views/DoubanBoxOffice.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, FlatList, TouchableOpacity, TextInput,Button} from 'react-native';
+import {API,API_LIST} from '../utils/constants.js'
+
+export default class DoubanBoxOffice extends Component {
+	constructor(props) {
+		super(props);
+		this.page = 1;
+		this.state = {
+			visuals: [],
+		}
+	}
+	componentDidMount() {
+	}
+	getVisuals(){
+		const {api,visuals} = this.state;
+		const url = api+this.page;
+		let results = [];
+		this.setState({loading:true});
+		fetch(url)
+		.then(res => res.json())
+		.then((res) => {
+			if (this.page == 1) {
+				results = res.results;
+			} else {
+				results = visuals.concat(res.results);
+			}
+			this.setState({
+				visuals:results,
+				loading:false
+			})
+		})
+		.catch((err) => {
+			console.error(err);
+		})
+	}
+  render() {
+    return (
+      <View>
+				<Text>douban box office</Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  
+});
