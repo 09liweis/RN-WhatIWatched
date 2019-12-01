30950a6690ab44c3e2080d8c52f7184ff50752f8
Sam Li <weisen.li@hotmail.com>
2019-12-01 11:50:08 -0500

    add douban us office box api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,11 +4,14 @@ const API_DETAIL = API + 'visual/';
 const API_INCREASE_EPISODE = API_DETAIL +'increase_episode?id=';
 const API_SEARCH = API + 'search?keyword=';
 const API_DOUBAN_DETAIL = 'https://api.douban.com/v2/movie/subject/{id}?apikey=0df993c66c0c636e29ecbb5344252a4a';
+const API_DOUBAN_USBO = 'https://api.douban.com/v2/movie/us_box?apikey=0df993c66c0c636e29ecbb5344252a4a';
+const API_MAOYAN = 'https://piaofang.maoyan.com/second-box';
 export {
   API,
   API_LIST,
   API_DETAIL,
   API_SEARCH,
   API_INCREASE_EPISODE,
-  API_DOUBAN_DETAIL
+  API_DOUBAN_DETAIL,
+  API_DOUBAN_USBO
 }
