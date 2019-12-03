5f5d1d540bee0643f7a459fcd9477a02a4f5337c
Sam Li <weisen.li@hotmail.com>
2019-12-02 22:49:26 -0500

    set boxoffice to tabnavigator

--- src/views/BoxOffice/index.js
+++ src/views/BoxOffice/index.js
@@ -1,27 +1,37 @@
 import React, {Component} from 'react';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import {Platform, StyleSheet, Text, View, FlatList, TouchableOpacity, TextInput,Button} from 'react-native';
 import Douban from './Douban';
+import Maoyan from './Maoyan';
 
-export default class BoxOffice extends Component {
-	constructor(props) {
-		super(props);
-		this.page = 1;
-		this.state = {
-			visuals: [],
+const BoxOffice = createBottomTabNavigator({
+  Douban: {
+		screen: Douban,
+		navigationOptions: {
+			title: "豆瓣",
+			tabBarIcon: ({ tintColor }) => (
+				<Icon
+					name="film"
+					size={17}
+					color={tintColor} />
+			)
+		}
+  },
+	Maoyan: {
+		screen: Maoyan,
+		navigationOptions: {
+			tabBarLabel: "猫眼",
+			tabBarIcon: ({ tintColor }) => (
+				<Icon
+					name="memory"
+					size={17}
+					color={tintColor} />
+			)
 		}
 	}
-	componentDidMount() {
-	}
-	
-  render() {
-    return (
-      <View>
-				<Text>box office</Text>
-				<Douban/>
-      </View>
-    );
-  }
-}
-const styles = StyleSheet.create({
-  
+},{
+	tabBarPosition:'top'
 });
+
+export default createStackNavigator({ BoxOffice }, { headerMode: "none" });
