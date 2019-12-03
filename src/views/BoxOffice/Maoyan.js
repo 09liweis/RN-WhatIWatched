e4084795e36da0ee2e033fd3bd773c75b60c2182
Sam Li <weisen.li@hotmail.com>
2019-12-02 21:32:49 -0500

    add maoyan views

---
+++ src/views/BoxOffice/Maoyan.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, FlatList,Image,ActivityIndicator,Dimensions} from 'react-native';
+import {API_MAOYAN} from '../../utils/constants.js'
+const {width,height} = Dimensions.get('window');
+export default class Maoyan extends Component {
+	constructor(props) {
+		super(props);
+		this.page = 1;
+		this.state = {
+			maoyan: {},
+			loading:false
+		}
+	}
+	componentDidMount() {
+		this.getMaoyan()
+	}
+	getMaoyan(){
+		this.setState({loading:true});
+		fetch(API_MAOYAN)
+		.then(res => res.json())
+		.then((res) => {
+			this.setState({maoyan:res,loading:false});
+		})
+		.catch((err) => {
+			console.error(err);
+		})
+	}
+  render() {
+		const {maoyan,loading} = this.state;
+		let view;
+		if (loading) {
+			view = <View style={styles.loading}><ActivityIndicator size="large" color="#0000ff" /></View>;
+		} else {
+			view = <React.Fragment>
+								
+								
+							</React.Fragment>
+		}
+    return (
+			<View style={styles.container}>
+				{view}
+			</View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+	container:{
+	},
+	loading:{
+		height,
+		alignItems:'center',
+		justifyContent:'center'
+	},
+	
+});
