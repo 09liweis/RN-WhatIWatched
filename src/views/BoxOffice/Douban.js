af84c6f7e046a053dbbde3d686c411ee7e1e4308
Sam Li <weisen.li@hotmail.com>
2019-12-02 21:12:37 -0500

    add loading

--- src/views/BoxOffice/Douban.js
+++ src/views/BoxOffice/Douban.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, FlatList,Image} from 'react-native';
+import {Platform, StyleSheet, Text, View, FlatList,Image,ActivityIndicator,Dimensions} from 'react-native';
 import {API_DOUBAN_USBO} from '../../utils/constants.js'
-
+const {width,height} = Dimensions.get('window');
 export default class Douban extends Component {
 	constructor(props) {
 		super(props);
 		this.page = 1;
 		this.state = {
-			douban: {}
+			douban: {},
+			loading:false
 		}
 	}
 	componentDidMount() {
@@ -18,39 +19,53 @@ export default class Douban extends Component {
 		fetch(API_DOUBAN_USBO)
 		.then(res => res.json())
 		.then((res) => {
-			console.log(res);
-			this.setState({douban:res});
+			this.setState({douban:res,loading:false});
 		})
 		.catch((err) => {
 			console.error(err);
 		})
 	}
   render() {
-		const {douban} = this.state;
+		const {douban,loading} = this.state;
+		let view;
+		if (loading) {
+			view = <View style={styles.loading}><ActivityIndicator size="large" color="#0000ff" /></View>;
+		} else {
+			view = <React.Fragment>
+							<Text style={styles.title}>{douban.title} {douban.date}</Text>
+							<FlatList
+								data={douban.subjects}
+								keyExtractor={item => item.subject.id.toString()}
+								renderItem={({item}) =>
+									<View style={styles.boxoffice}>
+										<Image style={styles.boxofficeImg} source={{uri:item.subject.images.small}}/>
+										<View style={styles.boxofficeInfo}>
+											<Text>{item.subject.title}</Text>
+											<Text>{item.subject.original_title}</Text>
+											<Text>{item.subject.durations.join(',')}</Text>
+											<Text>{item.subject.genres.join(',')}</Text>
+											<Text>${item.box}</Text>
+										</View>
+									</View>
+								}
+								/>
+						</React.Fragment>
+		}
     return (
-      <View>
-				<Text style={styles.title}>{douban.title} {douban.date}</Text>
-				<FlatList
-					data={douban.subjects}
-					keyExtractor={item => item.subject.id.toString()}
-					renderItem={({item}) =>
-						<View style={styles.boxoffice}>
-							<Image style={styles.boxofficeImg} source={{uri:item.subject.images.small}}/>
-							<View style={styles.boxofficeInfo}>
-								<Text>{item.subject.title}</Text>
-								<Text>{item.subject.original_title}</Text>
-								<Text>{item.subject.durations.join(',')}</Text>
-								<Text>{item.subject.genres.join(',')}</Text>
-								<Text>${item.box}</Text>
-							</View>
-						</View>
-					}
-					/>
-      </View>
+			<View style={styles.container}>
+				{view}
+			</View>
     );
   }
 }
 const styles = StyleSheet.create({
+	container:{
+	},
+	loading:{
+		height,
+		alignItems:'center',
+		justifyContent:'center'
+	},
 	title:{
 		textAlign:'center',
 		fontSize:20
