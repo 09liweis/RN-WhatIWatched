d967668e42b114395423e581d9e0a6655602475c
Sam Li <weisen.li@hotmail.com>
2019-12-02 21:16:18 -0500

    fix tabbing

--- src/views/BoxOffice/Douban.js
+++ src/views/BoxOffice/Douban.js
@@ -32,24 +32,24 @@ export default class Douban extends Component {
 			view = <View style={styles.loading}><ActivityIndicator size="large" color="#0000ff" /></View>;
 		} else {
 			view = <React.Fragment>
-							<Text style={styles.title}>{douban.title} {douban.date}</Text>
-							<FlatList
-								data={douban.subjects}
-								keyExtractor={item => item.subject.id.toString()}
-								renderItem={({item}) =>
-									<View style={styles.boxoffice}>
-										<Image style={styles.boxofficeImg} source={{uri:item.subject.images.small}}/>
-										<View style={styles.boxofficeInfo}>
-											<Text>{item.subject.title}</Text>
-											<Text>{item.subject.original_title}</Text>
-											<Text>{item.subject.durations.join(',')}</Text>
-											<Text>{item.subject.genres.join(',')}</Text>
-											<Text>${item.box}</Text>
+								<Text style={styles.title}>{douban.title} {douban.date}</Text>
+								<FlatList
+									data={douban.subjects}
+									keyExtractor={item => item.subject.id.toString()}
+									renderItem={({item}) =>
+										<View style={styles.boxoffice}>
+											<Image style={styles.boxofficeImg} source={{uri:item.subject.images.small}}/>
+											<View style={styles.boxofficeInfo}>
+												<Text>{item.subject.title}</Text>
+												<Text>{item.subject.original_title}</Text>
+												<Text>{item.subject.durations.join(',')}</Text>
+												<Text>{item.subject.genres.join(',')}</Text>
+												<Text>${item.box}</Text>
+											</View>
 										</View>
-									</View>
-								}
-								/>
-						</React.Fragment>
+									}
+									/>
+							</React.Fragment>
 		}
     return (
 			<View style={styles.container}>
