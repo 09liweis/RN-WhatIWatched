d0bdaf91c8f5dc7bc47dc261c00456dfbd45db03
Sam Li <weisen.li@hotmail.com>
2020-01-19 10:47:22 -0500

    add get imdb id api to constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,7 @@ const API_LIST = API + 'visuals/';
 const API_DETAIL = API + 'visual/';
 const API_INCREASE_EPISODE = API_DETAIL +'increase_episode?id=';
 const API_SEARCH = API + 'search?keyword=';
+const API_GET_IMDB_ID = API + 'visuals/get_imdb_id?douban_id='
 const API_DOUBAN_DETAIL = 'https://api.douban.com/v2/movie/subject/{id}'+API_DOUBAN_KEY;
 const API_DOUBAN_DETAIL_PHOTO = 'https://api.douban.com/v2/movie/subject/{id}/photos'+API_DOUBAN_KEY;
 const API_DOUBAN_USBO = 'https://api.douban.com/v2/movie/us_box'+API_DOUBAN_KEY;
@@ -19,5 +20,6 @@ export {
   API_DOUBAN_DETAIL_PHOTO,
   API_DOUBAN_USBO,
   API_MAOYAN,
-  API_MAOYAN_DETAIL
+  API_MAOYAN_DETAIL,
+  API_GET_IMDB_ID
 }
