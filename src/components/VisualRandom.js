0e32cfc5cfda30d51745f15ca049f13923fba167
Sam Li <weisen.li@hotmail.com>
2020-03-10 16:30:52 -0400

    implement visual random component

--- src/components/VisualRandom.js
+++ src/components/VisualRandom.js
@@ -1,2 +1,43 @@
 import React, {Component} from 'react';
-import {API,API_RANDOM} from '../utils/constants.js'
+import {Platform, StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
+import {API_RANDOM} from '../utils/constants.js'
+import VisualBasic from './VisualBasic'
+
+export default class VisualRandom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visual:{}
+    };
+  }
+  componentDidMount() {
+    fetch(API_RANDOM)
+		.then(res => res.json())
+		.then((res) => {
+			this.setState({visual:res.result});
+		})
+		.catch((err) => {
+			console.error(err);
+		})
+  }
+  render() {
+    const {navigation} = this.props;
+    const {visual} = this.state;
+    let visualView;
+    if (visual) {
+      visualView = (
+        <TouchableOpacity onPress={() => navigation.navigate('VisualDetail',{id:visual.id,title:visual.title})}>
+          <VisualBasic visual={visual} />
+        </TouchableOpacity>
+      );
+    } else {
+      visualView = <Text>Loading</Text>;
+    }
+    return(
+      <View>{visualView}</View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+
+});
