8808a51f741c955323d1a4054bbfb4945f79e839
Sam Li <weisen.li@hotmail.com>
2020-03-10 16:31:09 -0400

    refactor visualdetail view

--- src/views/VisualDetail.js
+++ src/views/VisualDetail.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text,ScrollView,View, Image, TouchableOpacity, Button,Linking,Dimensions} from 'react-native';
 import {API_DETAIL,API_INCREASE_EPISODE,API_DOUBAN_DETAIL,API_DOUBAN_DETAIL_PHOTO} from '../utils/constants.js'
 import { FlatList } from 'react-native-gesture-handler';
+import VisualBasic from '../components/VisualBasic.js';
 const {width,height} = Dimensions.get('window');
 
 export default class VisualDetail extends Component {
@@ -81,33 +82,15 @@ export default class VisualDetail extends Component {
     if (v.title) {
       res = (
         <ScrollView>
-          <View style={styles.header}>
-            <Image style={styles.poster} source={{uri:v.poster}}/>
-            <View style={styles.title}>
-              <Text style={styles.tl}>{v.title}</Text>
-              <Text style={styles.sbtl}>{v.original_title}</Text>
-              <TouchableOpacity style={styles.ratingRow} onPress={()=>Linking.openURL('https://movie.douban.com/subject/'+v.douban_id)}>
-                <Text style={[styles.douban,styles.ratingLabel]}>豆</Text>
-                <Text>{v.douban_rating}</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.ratingRow} onPress={()=>Linking.openURL('https://imdb.com/title/'+v.imdb_id)}>
-                <Text style={[styles.imdb,styles.ratingLabel]}>IMDB</Text>
-                <Text>{v.imdb_rating}</Text>
-              </TouchableOpacity>
-              <Text>{v.current_episode}/{v.episodes}</Text>
-              <Text>{v.release_date}</Text>
-              <Text>国家: {v.countries.join(',')}</Text>
-              <Text>语言: {v.languages.join(',')}</Text>
-              <Button 
-                onPress={this.increaseEpisode}
-                title="Increate Episode"
-              />
-              <Button 
-                onPress={this.edit}
-                title="Edit"
-              />
-            </View>
-          </View>
+          <VisualBasic visual={v} />
+          <Button 
+            onPress={this.increaseEpisode}
+            title="Increate Episode"
+          />
+          <Button 
+            onPress={this.edit}
+            title="Edit"
+          />
           <Text style={styles.summary}>{v.summary}</Text>
           {(douban.casts && douban.casts.length > 0)?
           <FlatList
@@ -154,45 +137,6 @@ const styles = StyleSheet.create({
   container:{
     marginLeft:10,
     marginRight:10
-  },
-  header:{
-    flexDirection:'row',
-  },
-  title:{
-    padding:10,
-    width:width - width/3
-  },
-  tl:{
-    fontSize:20,
-    fontWeight:'bold',
-    fontFamily:'monospace'
-  },
-  sbtl:{
-    fontSize:16,
-    color:'#bbc2cf'
-  },
-  poster:{
-    width:width/3,
-    height:250
-  },
-  ratingRow:{
-    flexDirection:'row',
-    alignItems:'center',
-    marginTop:5
-  },
-  ratingLabel:{
-    marginRight:5,
-    paddingLeft:4,
-    paddingRight:4,
-    paddingTop:2,
-    paddingBottom:2
-  },
-  douban:{
-    backgroundColor:'#187610',
-    color:'#fff',
-  },
-  imdb:{
-    backgroundColor:'#f5c617',
   },
   summary:{
     marginTop:20,
