05ac235aac9ed015fb9ebf18563d32faf8be3a9e
Sam Li <weisen.li@hotmail.com>
2020-03-16 21:24:16 -0400

    add douban upcomming api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,6 +9,7 @@ const API_GET_IMDB_ID = API + 'visuals/get_imdb_id?douban_id='
 const API_DOUBAN_DETAIL = 'https://api.douban.com/v2/movie/subject/{id}'+API_DOUBAN_KEY;
 const API_DOUBAN_DETAIL_PHOTO = 'https://api.douban.com/v2/movie/subject/{id}/photos'+API_DOUBAN_KEY;
 const API_DOUBAN_USBO = 'https://api.douban.com/v2/movie/us_box'+API_DOUBAN_KEY;
+const API_DOUBAN_UPCOMING = 'https://api.douban.com/v2/movie/coming_soon?apikey=0b2bdeda43b5688921839c8ecb20399b'
 const API_MAOYAN = 'https://piaofang.maoyan.com/second-box';
 const API_MAOYAN_DETAIL = 'http://m.maoyan.com/ajax/detailmovie?movieId={id}'
 export {
@@ -21,6 +22,7 @@ export {
   API_DOUBAN_DETAIL,
   API_DOUBAN_DETAIL_PHOTO,
   API_DOUBAN_USBO,
+  API_DOUBAN_UPCOMING,
   API_MAOYAN,
   API_MAOYAN_DETAIL,
   API_GET_IMDB_ID
