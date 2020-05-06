7dd969b27b995ac4e7b0ffd8a59a6cfc3c605494
Sam Li <weisen.li@hotmail.com>
2020-05-06 12:03:43 -0400

    maoyan tab to space

--- src/views/BoxOffice/Maoyan.js
+++ src/views/BoxOffice/Maoyan.js
@@ -3,86 +3,86 @@ import {Platform, StyleSheet, Text, View, FlatList,Image,ActivityIndicator,Dimen
 import {API_MAOYAN} from '../../utils/constants.js'
 const {width,height} = Dimensions.get('window');
 export default class Maoyan extends Component {
-	constructor(props) {
-		super(props);
-		this.page = 1;
-		this.state = {
-			maoyan: {},
-			loading:false,
-		}
-	}
-	componentDidMount() {
-		this.getMaoyan()
-		setInterval(()=>{
-			this.getMaoyan();
-		},3000);
-	}
-	getMaoyan(){
-		if (!this.state.maoyan.serverTime) {
-			this.setState({loading:true});
-		}
-		fetch(API_MAOYAN)
-		.then(res => res.json())
-		.then((res) => {
-			this.setState({maoyan:res.data,loading:false});
-		})
-		.catch((err) => {
-			console.error(err);
-		})
-	}
+  constructor(props) {
+    super(props);
+    this.page = 1;
+    this.state = {
+      maoyan: {},
+      loading:false,
+    }
+  }
+  componentDidMount() {
+    this.getMaoyan()
+    setInterval(()=>{
+      this.getMaoyan();
+    },3000);
+  }
+  getMaoyan(){
+    if (!this.state.maoyan.serverTime) {
+      this.setState({loading:true});
+    }
+    fetch(API_MAOYAN)
+    .then(res => res.json())
+    .then((res) => {
+      this.setState({maoyan:res.data,loading:false});
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+  }
   render() {
-		const {maoyan,loading} = this.state;
-		let view;
-		if (loading) {
-			view = <View style={styles.loading}><ActivityIndicator size="large" color="#0000ff" /></View>;
-		} else {
-			view = (
-				<React.Fragment>
-					<Text style={styles.title}>{maoyan.serverTime}</Text>
-					<FlatList
-						data={maoyan.list}
-						keyExtractor={item => item.movieId.toString()}
-						renderItem={({item}) =>
-							<View style={styles.maoyan}>
-								<View style={styles.boxofficeInfo}>
-									<Text style={styles.maoyanTl}>{item.movieName}</Text>
-									<Text>{item.releaseInfo}</Text>
-									<Text>{item.boxInfo}{maoyan.totalBoxUnitInfo}</Text>
-									<Text>{item.sumBoxInfo}</Text>
-								</View>
-							</View>
-						}
-						/>
-				</React.Fragment>
-			);
-		}
+    const {maoyan,loading} = this.state;
+    let view;
+    if (loading) {
+      view = <View style={styles.loading}><ActivityIndicator size="large" color="#0000ff" /></View>;
+    } else {
+      view = (
+        <React.Fragment>
+          <Text style={styles.title}>{maoyan.serverTime}</Text>
+          <FlatList
+            data={maoyan.list}
+            keyExtractor={item => item.movieId.toString()}
+            renderItem={({item}) =>
+              <View style={styles.maoyan}>
+                <View style={styles.boxofficeInfo}>
+                  <Text style={styles.maoyanTl}>{item.movieName}</Text>
+                  <Text>{item.releaseInfo}</Text>
+                  <Text>{item.boxInfo}{maoyan.totalBoxUnitInfo}</Text>
+                  <Text>{item.sumBoxInfo}</Text>
+                </View>
+              </View>
+            }
+            />
+        </React.Fragment>
+      );
+    }
     return (
-			<View style={styles.container}>
-				{view}
-			</View>
+      <View style={styles.container}>
+        {view}
+      </View>
     );
   }
 }
 const styles = StyleSheet.create({
-	container:{
-	},
-	loading:{
-		height,
-		alignItems:'center',
-		justifyContent:'center'
-	},
-	title:{
-		fontSize:30,
-		textAlign:'center',
-		margin:10
-	},
-	maoyan:{
-		padding:20,
-		borderBottomColor:'#ccc',
-		borderBottomWidth:1,
-		borderStyle:'solid'
-	},
-	maoyanTl:{
-		fontSize:24
-	}
+  container:{
+  },
+  loading:{
+    height,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  title:{
+    fontSize:30,
+    textAlign:'center',
+    margin:10
+  },
+  maoyan:{
+    padding:20,
+    borderBottomColor:'#ccc',
+    borderBottomWidth:1,
+    borderStyle:'solid'
+  },
+  maoyanTl:{
+    fontSize:24
+  }
 });
