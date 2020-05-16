5495b6564d0fa6a66f388448e1b17ab7e0caefd5
Sam Li <weisen.li@hotmail.com>
2020-05-16 15:45:35 -0400

    add dashboard view as home tab

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,34 +1,56 @@
 import React, {Component} from 'react';
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import Dashboard from '../views/Dashboard';
 import Visuals from '../views/Visuals';
-import BoxOffice from '../views/BoxOffice/index'
+import BoxOffice from '../views/BoxOffice/index';
+
+const navigationOptions = (title, iconName) => {
+  return {
+    navigation: title,
+    tabBarIcon: ({tintColor}) => {
+      <Icon name={iconName} size={17} color={tintColor} />
+    }
+  }
+}
 
 const Home = createBottomTabNavigator({
-  Visuals: {
-		screen: Visuals,
-		navigationOptions: {
-			title: "Visuals",
-			tabBarIcon: ({ tintColor }) => (
-				<Icon
-					name="film"
-					size={17}
-					color={tintColor} />
-			)
-		}
+  Dashboard: {
+    screen: Dashboard,
+    navigationOptions: {
+      tabBarLabel: "Home",
+      tabBarIcon: ({ tintColor }) => {
+        <Icon
+          name="home"
+          size={17}
+          color={tintColor} />
+      }
+    }
   },
-	BoxOffice: {
-		screen: BoxOffice,
-		navigationOptions: {
-			tabBarLabel: "Box Office",
-			tabBarIcon: ({ tintColor }) => (
-				<Icon
-					name="memory"
-					size={17}
-					color={tintColor} />
-			)
-		}
-	}
+  Visuals: {
+    screen: Visuals,
+    navigationOptions: {
+      tabBarLabel: "Visuals",
+      tabBarIcon: ({ tintColor }) => (
+        <Icon
+          name="film"
+          size={17}
+          color={tintColor} />
+      )
+    }
+  },
+  BoxOffice: {
+    screen: BoxOffice,
+    navigationOptions: {
+      tabBarLabel: "Box Office",
+      tabBarIcon: ({ tintColor }) => (
+        <Icon
+          name="memory"
+          size={17}
+          color={tintColor} />
+      )
+    }
+  }
 });
 
 //Issue: the tab navigator needs to be wrapped inside a stack navigator
