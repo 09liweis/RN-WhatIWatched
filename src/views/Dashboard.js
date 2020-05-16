81f0f0e1338456dd11ea1d5b8bb53d8f16579518
Sam Li <weisen.li@hotmail.com>
2020-05-16 15:45:25 -0400

    create dashboard view

---
+++ src/views/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Platform, StyleSheet, Text,ScrollView,View, Image, TouchableOpacity, Button,Dimensions} from 'react-native';
+
+function Dashboard() {
+  return (
+    <View><Text>Dashboard</Text></View>
+  )
+}
+export default Dashboard;
