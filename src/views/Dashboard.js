e2509454c4f27589ccd7d589862a2dce2b2cd88f
Sam Li <weisen.li@hotmail.com>
2020-05-16 17:31:17 -0400

    initial style of dashboard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Platform, StyleSheet, Text,ScrollView,View, Image, TouchableOpacity, Button,Dimensions} from 'react-native';
 
-function Dashboard() {
+const Dashboard = (props) => {
+  const [stats, setStats] = useState(0);
   return (
-    <View><Text>Dashboard</Text></View>
+    <View>
+      <Text style={styles.viewTitle}>Dashboard</Text>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  viewTitle: {
+    fontSize: 30,
+    textAlign: 'center'
+  }
+});
+
 export default Dashboard;
