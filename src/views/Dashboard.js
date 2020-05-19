fbfcfd1138ac8f1fcb1276a730cab3c15164cfe5
Sam Li <weisen.li@hotmail.com>
2020-05-18 22:07:13 -0400

    add stats data to dashboard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
 import {Platform, StyleSheet, Text,ScrollView,View, Image, TouchableOpacity, Button,Dimensions} from 'react-native';
 import VisualRandom from '../components/VisualRandom'
+import { API_STATS } from '../utils/constants.js'
 
 export default class Dashboard extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      stats:{}
+    }
   }
+  componentDidMount() {
+    fetch(API_STATS)
+    .then(res => res.json())
+    .then((res) => {
+      this.setState({
+        stats: res,
+        loading:false
+      })
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+  }
+
   render() {
     const {navigation} = this.props;
+    const {stats} = this.state;
     return (
-      <View>
+      <View style={styles.pageContainer}>
         <Text style={styles.viewTitle}>Dashboard</Text>
         <VisualRandom navigation={navigation}/>
+        <View style={styles.statsContainer}>
+          <Text style={[styles.statsBlock]}>Movie: {stats.movie}</Text>
+          <Text style={[styles.statsBlock]}>TV: {stats.tv}</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  pageContainer: {
+    padding: 10
+  },
   viewTitle: {
     fontSize: 30,
-    textAlign: 'center'
+    textAlign: 'center',
+    marginBottom: 20
+  },
+  statsContainer: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  statsBlock: {
+    color: '#fff',
+    fontSize: 30,
+    padding: 20,
+    width: '48%',
+    backgroundColor: '#ccc'
   }
 });
