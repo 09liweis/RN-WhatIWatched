10210020b9b6971ee0de560e2265e5f6bfa9ee94
Sam Li <weisen.li@hotmail.com>
2020-05-24 11:14:18 -0400

    add loading state to visual random

--- src/components/VisualRandom.js
+++ src/components/VisualRandom.js
@@ -8,22 +8,30 @@ export default class VisualRandom extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      visual:{}
+      visual:{},
+      loading: false
     };
   }
   componentDidMount() {
+    this.setState({loading:true})
     get(API_RANDOM, (err, res) => {
-      this.setState({visual:res.result});
+      this.setState({visual:res.result,loading:false});
     });
+  }
+  componentWillUnmount() {
+
   }
   render() {
     const {navigation} = this.props;
-    const {visual} = this.state;
+    const {visual, loading} = this.state;
     let visualView;
-    if (visual) {
+    if (!loading) {
       visualView = (
         <TouchableOpacity onPress={() => navigation.navigate('VisualDetail',{id:visual.id,title:visual.title})}>
           <VisualBasic visual={visual} />
+          <View>
+            <Text>Loading</Text>
+          </View>
         </TouchableOpacity>
       );
     } else {
