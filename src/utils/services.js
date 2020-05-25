aef1148f26724669ddb246ff9e2fbebac0a2b2d4
Sam Li <weisen.li@hotmail.com>
2020-05-24 22:32:20 -0400

    add visual upsert service

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,3 +1,7 @@
+import {getDoubanDetailAPI, getImdbRatingAPI, API_UPSERT} from './constants';
+import axios from 'axios';
+import qs from 'qs'
+
 export function get(url, cb) {
   fetch(url)
   .then(res => res.json())
@@ -8,6 +12,50 @@ export function get(url, cb) {
     return cb(err, null);
   })
 }
-export function updateVisual(visual, cb) {
-  
+export function post(url, data, cb) {
+  // const params = new URLSearchParams();
+  // for (let k in data) {
+  //   if (data[k]) {
+  //     params.append(k,data[k]);
+  //   }
+  // }
+  // // console.log(params.toString());
+  // fetch(url, {method:'POST',body:params}).then((res) => {
+  //   console.log(res);
+  //   return cb(null, res);
+  // }).catch((err) => {
+  //   console.log(err);
+  //   return cb(err, null);
+  // });
+  axios({
+    method: 'post',
+    headers:{
+      'accept': 'application/json',
+      'Content-Type':'application/x-www-form-urlencoded'
+    },
+    url,
+    data: qs.stringify(data)//for django request post issue
+  }).then(res => {
+    return cb(null, res);
+  });
 }
+export function updateVisual(visual, cb) {
+  get(getDoubanDetailAPI(visual.douban_id), (err, d) => {
+    if (rating = d.rating.average) {
+      visual.douban_rating = rating;
+    }
+    if (release_date = d.pubdate) {
+      visual.release_date = release_date;
+    }
+    if (imdb_id = visual.imdb_id) {
+      get(getImdbRatingAPI(imdb_id), (err, imdb) => {
+        if (imdb_rating = imdb.imdb_rating) {
+          visual.imdb_rating = imdb_rating;
+        }
+        post(API_UPSERT, visual, (err, ret) => {
+          return cb(err, ret);
+        });
+      });
+    }
+  });
+};
