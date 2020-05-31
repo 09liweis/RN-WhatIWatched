660a96e8f5ccf0c8e49d47cb9df423be94a42712
Sam Li <weisen.li@hotmail.com>
2020-05-30 21:42:41 -0400

    remove icon from home page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,54 +1,26 @@
 import React, {Component} from 'react';
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
-import Icon from 'react-native-vector-icons/FontAwesome5';
 import Dashboard from '../views/Dashboard';
 import Visuals from '../views/Visuals';
 import BoxOffice from '../views/BoxOffice';
-
-const navigationOptions = (title, iconName) => {
-  return {
-    navigation: title,
-    tabBarIcon: ({tintColor}) => {
-      <Icon name={iconName} size={17} color={tintColor} />
-    }
-  }
-}
 
 const Home = createBottomTabNavigator({
   Dashboard: {
     screen: Dashboard,
     navigationOptions: {
-      tabBarLabel: "Home",
-      tabBarIcon: ({ tintColor }) => {
-        <Icon
-          name="home"
-          size={17}
-          color={tintColor} />
-      }
+      tabBarLabel: "Home"
     }
   },
   Visuals: {
     screen: Visuals,
     navigationOptions: {
-      tabBarLabel: "Visuals",
-      tabBarIcon: ({ tintColor }) => (
-        <Icon
-          name="film"
-          size={17}
-          color={tintColor} />
-      )
+      tabBarLabel: "Visuals"
     }
   },
   BoxOffice: {
     screen: BoxOffice,
     navigationOptions: {
-      tabBarLabel: "Box Office",
-      tabBarIcon: ({ tintColor }) => (
-        <Icon
-          name="memory"
-          size={17}
-          color={tintColor} />
-      )
+      tabBarLabel: "Box Office"
     }
   }
 });
