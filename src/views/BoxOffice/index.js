44eb38e76c6053c6b03be054f7b98f85c5289fd5
Sam Li <weisen.li@hotmail.com>
2020-05-30 21:51:06 -0400

    remove maoyan from boxoffice

--- src/views/BoxOffice/index.js
+++ src/views/BoxOffice/index.js
@@ -1,23 +1,5 @@
 import React, {Component} from 'react';
-import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 import Douban from './Douban';
 import Maoyan from './Maoyan';
 
-const BoxOffice = createBottomTabNavigator({
-  Douban: {
-    screen: Douban,
-    navigationOptions: {
-      title: "豆瓣"
-    }
-  },
-  Maoyan: {
-    screen: Maoyan,
-    navigationOptions: {
-      tabBarLabel: "猫眼"
-    }
-  }
-},{
-  tabBarPosition:'top'
-});
-
-export default createStackNavigator({ BoxOffice }, { headerMode: "none" });
+export default Douban;
