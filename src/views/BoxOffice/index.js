896794347818e0c2bff400f6d0746c1f79aa7df4
Sam Li <weisen.li@hotmail.com>
2020-05-30 21:39:11 -0400

    remove icon from box office

--- src/views/BoxOffice/index.js
+++ src/views/BoxOffice/index.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
-import Icon from 'react-native-vector-icons/FontAwesome5';
 import Douban from './Douban';
 import Maoyan from './Maoyan';
 
@@ -8,25 +7,13 @@ const BoxOffice = createBottomTabNavigator({
   Douban: {
     screen: Douban,
     navigationOptions: {
-      title: "豆瓣",
-      tabBarIcon: ({ tintColor }) => (
-        <Icon
-          name="film"
-          size={17}
-          color={tintColor} />
-      )
+      title: "豆瓣"
     }
   },
   Maoyan: {
     screen: Maoyan,
     navigationOptions: {
-      tabBarLabel: "猫眼",
-      tabBarIcon: ({ tintColor }) => (
-        <Icon
-          name="memory"
-          size={17}
-          color={tintColor} />
-      )
+      tabBarLabel: "猫眼"
     }
   }
 },{
