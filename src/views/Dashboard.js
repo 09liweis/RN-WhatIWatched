b060c7f5cf90a0abcdac00656bf8f681063f7a79
Sam Li <weisen.li@hotmail.com>
2020-07-29 12:02:48 -0400

    remove dump text

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -31,8 +31,6 @@ export default class Dashboard extends Component {
           <Text style={[styles.statsBlock]}>Movie: {stats.movie}</Text>
           <Text style={[styles.statsBlock]}>TV: {stats.tv}</Text>
         </View>
-        <View><Text>Year Data</Text></View>
-        <View><Text>Language Data</Text></View>
         <TouchableOpacity onPress={() => navigation.navigate('VisualForm')}>
           <Text style={styles.addNew}>Add New</Text>
         </TouchableOpacity>
