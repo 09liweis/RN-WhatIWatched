afd9fddb8f4c48473337c2ddf93548816b93573a
Sam Li <weisen.li@hotmail.com>
2021-05-22 12:56:58 -0400

    fix maoyan component with new api

--- src/views/BoxOffice/Maoyan.js
+++ src/views/BoxOffice/Maoyan.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, FlatList,Image,ActivityIndicator,Dimensions} from 'react-native';
 import {API_MAOYAN} from '../../utils/constants.js'
+import {post} from '../../utils/services';
 const {width,height} = Dimensions.get('window');
 export default class Maoyan extends Component {
   constructor(props) {
@@ -8,6 +9,7 @@ export default class Maoyan extends Component {
     this.page = 1;
     this.state = {
       maoyan: {},
+      movies:[],
       loading:false,
     }
   }
@@ -15,40 +17,35 @@ export default class Maoyan extends Component {
     this.getMaoyan()
     setInterval(()=>{
       this.getMaoyan();
-    },3000);
+    },10000);
   }
   getMaoyan(){
-    if (!this.state.maoyan.serverTime) {
+    if (this.state.movies.length == 0) {
       this.setState({loading:true});
     }
-    fetch(API_MAOYAN)
-    .then(res => res.json())
-    .then((res) => {
-      this.setState({maoyan:res.data,loading:false});
-    })
-    .catch((err) => {
-      console.error(err);
+    post(API_MAOYAN,{},(err,ret)=> {
+      this.setState({movies:ret.movies,loading:false});
     })
   }
   render() {
-    const {maoyan,loading} = this.state;
+    const {maoyan,loading,movies} = this.state;
     let view;
     if (loading) {
       view = <View style={styles.loading}><ActivityIndicator size="large" color="#0000ff" /></View>;
     } else {
       view = (
         <React.Fragment>
-          <Text style={styles.title}>{maoyan.serverTime}</Text>
+          <Text style={styles.title}>猫眼票房</Text>
           <FlatList
-            data={maoyan.list}
-            keyExtractor={item => item.movieId.toString()}
+            data={movies}
+            keyExtractor={item => item.movieInfo.movieId.toString()}
             renderItem={({item}) =>
               <View style={styles.maoyan}>
                 <View style={styles.boxofficeInfo}>
-                  <Text style={styles.maoyanTl}>{item.movieName}</Text>
-                  <Text>{item.releaseInfo}</Text>
-                  <Text>{item.boxInfo}{maoyan.totalBoxUnitInfo}</Text>
-                  <Text>{item.sumBoxInfo}</Text>
+                  <Text style={styles.maoyanTl}>{item.movieInfo.movieName}</Text>
+                  <Text>{item.movieInfo.releaseInfo}</Text>
+                  <Text>分账票房: {item.sumSplitBoxDesc}</Text>
+                  <Text>总票房: {item.sumBoxDesc}</Text>
                 </View>
               </View>
             }
@@ -73,6 +70,7 @@ const styles = StyleSheet.create({
   },
   title:{
     fontSize:30,
+    fontWeight: 'bold',
     textAlign:'center',
     margin:10
   },
